95c0ed19089bbb5c2ca4d5c1d6464a973d0e7222
DominikScholz <dominik.scholz@live.at>
2025-08-12 00:10:27 +0200

    better icon style

--- content.js
+++ content.js
@@ -486,7 +486,8 @@
     const button = document.createElement("button");
     button.type = "button";
     button.className = BUTTON_CLASS;
-    button.textContent = "Create issue";
+    // from Phosphor Icon under MIT license: https://phosphoricons.com/
+    button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"/><circle cx="156" cy="92" r="12" fill="currentColor" /><circle cx="100" cy="92" r="12" fill="currentColor" /><line x1="128" y1="128" x2="128" y2="224" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/><path d="M208,144a80,80,0,0,1-160,0V112a80,80,0,0,1,160,0Z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/><line x1="232" y1="184" x2="203.18" y2="171.41" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/><line x1="232" y1="72" x2="203.18" y2="84.59" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/><line x1="24" y1="72" x2="52.82" y2="84.59" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/><line x1="24" y1="184" x2="52.82" y2="171.41" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/><line x1="16" y1="128" x2="240" y2="128" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/></svg>'
     button.addEventListener("click", (event) => {
       event.stopPropagation();
       event.preventDefault();
